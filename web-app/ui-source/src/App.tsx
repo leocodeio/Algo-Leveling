2dbed8dfc5c1303ca95f64baeec7c1ef576cc7e1
Harshax <basicleo787@gmail.com>
2024-10-31 23:22:10 +0530

    some refactor at frontend

--- web-app/ui-source/src/App.tsx
+++ web-app/ui-source/src/App.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Entry from "./components/Entry";
+import Credits from "./components/Credits";
 import Dummy from "./components/Dummy";
-
+import Header from "./utils/common/Header";
+import Entry from "./components/Entry";
 function App() {
   return (
-    <div className="App">
+    <div className="h-screen w-full">
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Entry />} />
+          <Route path="/credits" element={<Credits />} />
           <Route path="/dummy" element={<Dummy />} />
         </Routes>
       </BrowserRouter>
--- web-app/ui-source/src/components/Entry.tsx
+++ web-app/ui-source/src/components/Entry.tsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 const Entry = () => {
   return (
-    <div className="h-screen flex flex-col items-center justify-center text-center font-sans p-4">
-      Welcome - developed by catalyst community -<b>@leocodeio</b>
+    <div className="h-full flex flex-col items-center justify-center text-center font-sans p-4">
+      Entry to the application
       <Link
         className="border border-2 border-black px-2 py-1 rounded"
         to="/dummy"
       >
-        Home
+        Sample
       </Link>
     </div>
   );
